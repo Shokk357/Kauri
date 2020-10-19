94b7d391fd1d5fc17253781c401b091d827fa969
Shokk357 <pavcygan17@gmail.com>
2020-10-20 00:20:34 +0300

    Network implementation by ACE and multithreading server support

--- Server/server.cpp
+++ Server/server.cpp
@@ -1,37 +1,13 @@
-#include <sys/socket.h>
-#include <unistd.h>
 #include <iostream>
-#include <netinet/in.h>
-#include <unordered_map>
-#include "../ASN/ASN.h"
+#include <ace/SOCK_Acceptor.h>
+#include <ace/Thread_Manager.h>
+#include <ace/Reactor.h>
+#include <ace/Svc_Handler.h>
+#include <thread>
+#include <list>
+#include "server.h"
 
 int main() {
-
-    sockaddr_in server_address;
-    server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(1234);
-    server_address.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
-
-    int server_sockfd = socket(AF_INET, SOCK_STREAM, 0);
-
-    bind(server_sockfd, (struct sockaddr *) &server_address, sizeof(server_address));
-
-    listen(server_sockfd, 5);
-
-    while (true) {
-        int client_sockfd = accept(server_sockfd, nullptr, nullptr);
-        int packLen;
-        recv(client_sockfd, &packLen, 4, 0);
-        uint8_t *s = new uint8_t[packLen];
-        recv(client_sockfd, s, packLen, 0);
-        ASN<std::vector<int>> elem;
-        std::vector<int> message = elem.decode(s);
-        for (auto elem : message) {
-            std::cout << elem << ' ';
-        }
-        std::cout << std::endl;
-        delete[] s;
-        shutdown(client_sockfd, SHUT_RDWR);
-        close(client_sockfd);
-    }
+    server server(1234);
+    server.handlingLoop();
 }
